a9cd450689b756bff9a4c720272c3bc2a0c67d3e
Vlad Dubov <dubov@betaagency.ru>
2015-08-12 17:14:40 +0300

    Read me

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    app = {
+    	js : ['www/public/js/utils.js', 'www/public/js/main.js']
+    }
 
 gulp.task('connect', function() {
   connect.server({
@@ -13,9 +17,15 @@ gulp.task('html', function () {
   gulp.src('./www/*.html')
     .pipe(connect.reload());
 });
- 
+
+gulp.task('js', function () {
+  gulp.src(app.js)
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./www/public/js'));
+});
+
 gulp.task('watch', function () {
   gulp.watch(['./www/*.html'], ['html']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['js','connect', 'watch']);
